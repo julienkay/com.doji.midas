1f24ad63894fbf0d91c6f0dcdcccb9b23827cce5
Julien Kipp <julien@doji-tech.com>
2024-08-17 15:15:54 +0200

    bump version

--- com.doji.midas/CHANGELOG.md
+++ com.doji.midas/CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.1.0] - 2024-08-17
+
+### Changed
+
+- Updated Sentis dependency to v1.6.0-pre.1
+- Normalization is now handled by a separate class using the Functional API
+
 ## [1.0.1] - 2024-02-09
 
 ### Added
--- com.doji.midas/Runtime/AssemblyInfo.cs
+++ com.doji.midas/Runtime/AssemblyInfo.cs
@@ -1,3 +1,13 @@
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyTitle("Doji.Midas")]
+[assembly: AssemblyDescription("Monocular Depth Estimation with Unity Sentis using MiDaS models")]
+[assembly: AssemblyCompany("Doji Technologies")]
+[assembly: AssemblyProduct("Doji.Midas")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2024")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 [assembly: InternalsVisibleTo("Doji.Midas.Editor")]
